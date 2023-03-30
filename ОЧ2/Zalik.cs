da648db25243cb0117c30929e5a4d280cfbc0787
thevictoriax <viktoriia.makukh@lnu.edu.ua>
2023-03-30 22:00:39 +0300

    add events

--- ОЧ2/Zalik.cs
+++ ОЧ2/Zalik.cs
@@ -8,24 +8,62 @@ namespace ОЧ2
 {
     public class Zalik: IComparable<Zalik>, ICloneable
     {
-        public string Subject { get; protected set; }
-        public string Teacher { get; set; }
         public int Grade { get; set; }
+        public string Teacher { get; set; }
+        private string subject;
 
+        public virtual string Subject
+        {
+            get 
+            { 
+                if (Grade > 50 && Grade < 75) 
+                { 
+                    OnGradeSatisfactory(EventArgs.Empty); 
+                }
+                if (Grade > 74 && Grade < 90)
+                {
+                    OnGradeGood(EventArgs.Empty);
+                }
+                if (Grade > 89)
+                {
+                    OnGradePerfect(EventArgs.Empty);
+                }
+                return subject; 
+            }
+            set 
+            { 
+                subject = value; 
+            }
+        }
+
+        public event EventHandler GradeSatisfactory;
+        public event EventHandler GradeGood;
+        public event EventHandler GradePerfect;
 
         public Zalik()
         {
-            Subject = string.Empty;
+            subject = string.Empty;
             Teacher = string.Empty; 
             Grade = 0;
+
+            //SubscribeToGradeSatisfactoryEvent();
+            //SubscribeToGradeGoodEvent();
+            //SubscribeToGradePerfectEvent();
         }
            
 
+
         public Zalik(string _subject, string _teacher, int _grade) 
         {
+            GradeSatisfactory += ThisGradeSatisfactory;
             Grade = _grade >= 100 ? 100 : _grade;
-            Subject = _subject;
+            subject = _subject;
             Teacher = _teacher;
+
+            SubscribeToGradeSatisfactoryEvent();
+            SubscribeToGradeGoodEvent();
+            SubscribeToGradePerfectEvent();
+
         }
 
         public int CompareTo(Zalik other)
@@ -35,7 +73,7 @@ namespace ОЧ2
 
         public object Clone()
         {
-            Zalik clone = new Zalik(Subject, "", 0);
+            Zalik clone = new Zalik(subject, "", 0);
             return clone;
         }
 
@@ -71,13 +109,13 @@ namespace ОЧ2
 
         public override string ToString()
         {
-            if (Grade>50)
+            if (Grade >50)
             {
-                return $"{Subject} , {Teacher} , {Grade} - зараховано ";
+                return $"{subject} , {Teacher} , {Grade} - зараховано ";
             }
             else
             {
-                return $"{Subject} , {Teacher} , {Grade} - НЕ зараховано ";
+                return $"{subject} , {Teacher} , {Grade} - НЕ зараховано ";
             }
         }
 
@@ -93,5 +131,52 @@ namespace ОЧ2
                 return Grade == zlk.Grade;
             }
         }
+        private void SubscribeToGradeSatisfactoryEvent()
+        {
+            GradeSatisfactory += ThisGradeSatisfactory;
+        }
+
+        protected virtual void OnGradeSatisfactory(EventArgs e)
+        {
+            GradeSatisfactory?.Invoke(this, e);
+        }
+
+        protected virtual void ThisGradeSatisfactory(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Дисципліну здано задовільно!");
+            Console.ResetColor();
+        }
+        private void SubscribeToGradeGoodEvent()
+        {
+            GradeGood += ThisGradeGood;
+        }
+        protected virtual void OnGradeGood(EventArgs e)
+        {
+            GradeGood?.Invoke(this, e);
+        }
+
+        protected virtual void ThisGradeGood(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Дисципліну здано добре!");
+            Console.ResetColor();
+        }
+        private void SubscribeToGradePerfectEvent()
+        {
+            GradePerfect += ThisGradePerfect;
+        }
+        protected virtual void OnGradePerfect(EventArgs e)
+        {
+            GradePerfect?.Invoke(this, e);
+        }
+
+        protected virtual void ThisGradePerfect(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Дисципліну здано відмінно!");
+            Console.ResetColor();
+        }
+
     }
 }
